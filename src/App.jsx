44701dea9e65fbbd86212fcc8152267254cc05f4
AydanKusland <chinatugov@mail.ru>
2023-05-16 18:18:01 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Landing, Error, Register, Dashboard } from './pages'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='landing' element={<Landing />} />
+				<Route path='register' element={<Register />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
+
+export default App
---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard'
+import Landing from './Landing'
+import Error from './ErrorPage'
+import Register from './Register'
+
+export { Dashboard, Landing, Error, Register }
